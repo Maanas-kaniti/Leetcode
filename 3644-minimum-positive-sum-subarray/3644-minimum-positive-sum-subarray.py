98a292e7db217692ebafe6ed0b0d96673217a657
Maanas Kaniti <150051799+DemonicEmperor@users.noreply.github.com>
2025-07-17 16:24:30 +0530

    Time: 23 ms (72.4%), Space: 17.9 MB (42.64%) - LeetHub

--- 3644-minimum-positive-sum-subarray/3644-minimum-positive-sum-subarray.py
+++ 3644-minimum-positive-sum-subarray/3644-minimum-positive-sum-subarray.py
@@ -1,14 +1,16 @@
 class Solution:
     def minimumSumSubarray(self, nums: List[int], l: int, r: int) -> int:
         n = len(nums)
-        m = float("inf")
-        for i in range(l,r+1):
-            for j in range(n-i+1):
-                sub = nums[j:j+i]
-                if sum(sub)>0:
-                    m = min(m,sum(sub))
-        
-        if m!=float("inf"):
-            return m
-        else:
-            return -1
+        ans = float('inf')
+
+        for window_size in range(l, r + 1):
+            window_sum = sum(nums[:window_size])
+            if window_sum > 0:
+                ans = min(ans, window_sum)
+
+            for i in range(window_size, n):
+                window_sum += nums[i] - nums[i - window_size]
+                if window_sum > 0:
+                    ans = min(ans, window_sum)
+
+        return -1 if ans == float('inf') else ans
