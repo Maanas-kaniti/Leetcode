94606f7445cf0f05e33b77d0b00ef92a0b1f551a
Maanas Kaniti <150051799+DemonicEmperor@users.noreply.github.com>
2025-07-19 11:41:45 +0530

    Time: 12 ms (12.46%), Space: 18.1 MB (6.27%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.py
@@ -3,14 +3,10 @@ class Solution:
         i=j=0
         n = len(nums)
         c = 0
-        while i<n:
-            if j<n:
-                sub = nums[i:j+1]
-                if len(sub)==3:
-                    if sub[0]+sub[-1] == sub[1]/2:
-                        c+=1
-                j+=1
-            else:
-                i+=1
-                j=i
+        k=3
+        for i in range(n-k+1):
+            sub = nums[i:i+k]
+            if len(sub)==3:
+                if sub[0]+sub[-1]==sub[1]/2:
+                    c+=1
         return c
