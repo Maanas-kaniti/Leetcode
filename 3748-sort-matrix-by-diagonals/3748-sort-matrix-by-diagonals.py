70c39d8073883aeb8e69a316e94dd39b78a947b8
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2025-08-28 15:47:39 +0530

    Time: 27 ms (10.42%), Space: 18 MB (29.58%) - LeetHub

---
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        diag = defaultdict(list)
+        for i in range(n):
+            for j in range(n):
+                diag[i-j].append(grid[i][j])
+        print(diag)
+        for key in diag:
+            if key>=0:
+                diag[key].sort(reverse= True)
+            else:
+                diag[key].sort()
+        print(diag)
+        for i in range(n):
+            for j in range(n):
+                grid[i][j] = diag[i-j].pop(0)
+        return grid
