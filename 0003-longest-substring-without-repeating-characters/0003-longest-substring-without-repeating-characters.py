35681b04e7fbcbf463608bebe7c47938515788b6
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2025-09-01 20:12:49 +0530

    Time: 1558 ms (5.01%), Space: 18.1 MB (12.12%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,41 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # d = {}
-        # k = 1
-        # n = len(s)
-        # i = 0
-        # j = 0 
-        # if n==0:
-        #     return 0
-        # while i<n:
-        #     if j<n:
-        #         sub = s[i:j+1]
-        #         if len(set(sub))==len(sub):
-        #             d[sub]= len(sub)
-        #         j+=1
-        #     else:
-        #         i+=1
-        #         j=i
-        # return max(d.values())
+        i = 0
+        j = 0
+        m = 0
+        l = 0
+        myset = []
+        while i<len(s):
+            if j<len(s) and (s[j] not in myset):
+                myset.append(s[j])
+                j+=1
+                l+=1
+                m = max(m,l)
+            else:
+                myset = []
+                i+=1
+                j = i
+                l = 0
+        return m
         
-        k=1
-        res=""
-        n=len(s)
-        m = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-        if m in s:
-            return 95
-        if n>=0 and n<=5*(10**4):
-            if len(s)==1:
-                return 1
-          
-            for j in range(n):
-                for i in range(n-k+1):
-                    sub=s[i:i+k]
-                    if len(set(sub))==len(sub):
-                        res=sub
-                        break
-                k=k+1
-                
-            return len(res)
-        
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
