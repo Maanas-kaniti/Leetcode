f889c9ebbf74ee193c1c06b69dab87d3c3fc6a68
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-04-17 15:09:04 +0530

    Time: 13 ms (80.09%), Space: 47.9 MB (22.26%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,60 +1,17 @@
-// import java.util.*;
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//         int[] res = new int[k];
-//         for(int num : nums){
-//             map.put(num,map.getOrDefault(num,0)+1);
-//         }
-//         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
-//         pq.addAll(map.entrySet());
-//         for(int i = 0;i<k;i++){
-//             Map.Entry<Integer,Integer> entry = pq.poll();
-//             res[i] = entry.getKey();
-//         }
-//         return res;
-//     }
-// }
 import java.util.*;
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-
-        // Count frequency
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] res = new int[k];
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-
-        // Bucket array: index = frequency
-        List<Integer>[] buckets = new ArrayList[nums.length + 1];
-
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            int num = entry.getKey();
-            int freq = entry.getValue();
-
-            if (buckets[freq] == null) {
-                buckets[freq] = new ArrayList<>();
-            }
-
-            buckets[freq].add(num);
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.entrySet());
+        for(int i = 0;i<k;i++){
+            Map.Entry<Integer,Integer> entry = pq.poll();
+            res[i] = entry.getKey();
         }
-
-        // Collect top k frequent elements
-        int[] result = new int[k];
-        int idx = 0;
-
-        for (int i = buckets.length - 1; i >= 0 && idx < k; i--) {
-            if (buckets[i] != null) {
-                for (int num : buckets[i]) {
-                    result[idx++] = num;
-                    if (idx == k) {
-                        return result;
-                    }
-                }
-            }
-        }
-
-        return result;
+        return res;
     }
 }
