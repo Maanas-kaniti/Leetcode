8f7f5b10839df80c5e5fa77e844b920a3dab445b
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-05-14 10:55:01 +0530

    Time: 6 ms (15.24%), Space: 45.2 MB (20.19%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public boolean isGood(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int n:nums){
+            max = Math.max(n,max);
+        }
+        if(nums.length!=max+1){
+            return false;
+        }
+        Arrays.sort(nums);
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+        for(int n : nums){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        int n = (max-1)*(max)/2;
+        int sum=0;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getKey()!=max && entry.getValue()==1){
+                sum+=entry.getKey();
+            }
+        }
+        return sum==n && map.get(max)==2;
+    }
+}
