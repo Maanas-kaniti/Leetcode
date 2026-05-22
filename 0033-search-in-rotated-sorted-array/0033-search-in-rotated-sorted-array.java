55fe59b8326583616d76e2ef7b78aa07f7183ff6
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-05-22 12:12:45 +0530

    Time: 0 ms (100%), Space: 44.1 MB (10.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,71 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int s = 0;
-        int e = nums.length-1;
-        int res = bs(nums,target,s,e);
-        return res;
-    }
-    static int bs(int [] arr , int t , int s , int e){
-        if(s>e){
-            return -1;
-        }
-        int m = (s+e)/2;
-        if(arr[m]==t){
-            return m;
-        }
-        if(arr[s]<= arr[m]){
-            if(t>=arr[s] && t<arr[m]){
-                return bs(arr,t,s,m-1);
+    public int search(int[] arr, int target) {
+             int left = 0;
+        int right = arr.length-1;
+        int res = -1;
+        if(left>right) return -1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(arr[mid]==target){
+                return mid;
             }
-            else return bs(arr,t,m+1,e);
-        }
-        if(t>arr[m] && t<=arr[e]){
-            return bs(arr,t,m+1,e);
-        }
-        else return bs(arr,t,s,m-1);
+             if (arr[left] <= arr[mid]) {
+
+                if (target >= arr[left] && target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            else {
+
+                if (target > arr[mid] && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }        
+        return -1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int s = 0;
+        // int e = nums.length-1;
+        // int res = bs(nums,target,s,e);
+        // return res;
     }
+    // static int bs(int [] arr , int t , int s , int e){
+    //     if(s>e){
+    //         return -1;
+    //     }
+    //     int m = (s+e)/2;
+    //     if(arr[m]==t){
+    //         return m;
+    //     }
+    //     if(arr[s]<= arr[m]){
+    //         if(t>=arr[s] && t<arr[m]){
+    //             return bs(arr,t,s,m-1);
+    //         }
+    //         else return bs(arr,t,m+1,e);
+    //     }
+    //     if(t>arr[m] && t<=arr[e]){
+    //         return bs(arr,t,m+1,e);
+    //     }
+    //     else return bs(arr,t,s,m-1);
+    // }
 }
