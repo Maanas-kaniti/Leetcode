fcd19c009e6de30c1674edef8213ece96376126e
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-06-03 19:19:15 +0530

    Time: 1 ms (100%), Space: 46.8 MB (77.34%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,18 +1,20 @@
 import java.util.*;
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int min = Integer.MAX_VALUE;
+        int min_land = Integer.MAX_VALUE;
         for(int i = 0;i<landStartTime.length;i++){
-            int land = landStartTime[i]+landDuration[i];
-            for(int j = 0;j<waterStartTime.length;j++){
-                min = Math.min(min , Math.max(land,waterStartTime[j])+waterDuration[j]);
-            }
+            min_land = Math.min(min_land,landStartTime[i]+landDuration[i]);
         }
+        int min_water = Integer.MAX_VALUE;
         for(int i = 0;i<waterStartTime.length;i++){
-            int water = waterStartTime[i]+waterDuration[i];
-            for(int j = 0;j<landStartTime.length;j++){
-                min = Math.min(min , Math.max(water,landStartTime[j])+landDuration[j]);
-            }
+            min_water = Math.min(min_water,waterStartTime[i]+waterDuration[i]);
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i<waterStartTime.length;i++){
+            min=Math.min(min , Math.max(min_land,waterStartTime[i])+waterDuration[i]); 
+        }
+        for(int i = 0;i<landStartTime.length;i++){
+            min = Math.min(min , Math.max(min_water,landStartTime[i])+landDuration[i]);
         }
         return min;
     }
