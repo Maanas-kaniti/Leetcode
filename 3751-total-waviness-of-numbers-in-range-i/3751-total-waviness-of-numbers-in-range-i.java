5f1878bf515f39f9d13d19a78b0f9fc153cbd53a
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-06-05 19:02:45 +0530

    Time: 26 ms (73.36%), Space: 46.7 MB (8.03%) - LeetHub

---
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    static int waves(int n){
+        int count = 0;
+        if(n<101) return count;
+        String s = String.valueOf(n);
+        for(int i = 0;i<s.length()-1;i++){
+            if(i!=0 && i!=s.length()-1){
+                int ch = s.charAt(i);
+                int prev = s.charAt(i-1);
+                int next = s.charAt(i+1);
+                if((ch>prev && ch>next)){
+                    count++;
+                }
+                else if(ch<prev && ch<next) count++;
+            }
+        }
+        return count;
+    }
+    public int totalWaviness(int num1, int num2) {
+        int waviness = 0;
+        for(int i = num1;i<num2+1;i++){
+            int wave = waves(i);
+            waviness+=wave;
+        }
+        return waviness;
+    }
+}
