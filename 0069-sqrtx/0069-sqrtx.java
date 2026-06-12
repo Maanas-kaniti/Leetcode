3fb79210d17e930c5225ae30e735613ae1ccaca3
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-06-12 09:49:38 +0530

    Time: 2 ms (11.27%), Space: 42.8 MB (8.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -4,10 +4,10 @@ class Solution {
         int r = x;
         while(l<=r){
             int mid = l+(r-l)/2;
-            if(mid*mid==x){
+            if((long)mid*mid==x){
                 return mid;
             }
-            else if(mid*mid<=x){
+            else if((long)mid*mid<x){
                 l = mid +1;
             }
             else r = mid-1;
