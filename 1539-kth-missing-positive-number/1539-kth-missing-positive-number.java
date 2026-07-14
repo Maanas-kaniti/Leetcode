21a810f6d4eb851331d7457b7e013b8f63c911f6
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-07-14 16:39:25 +0530

    Time: 0 ms (100%), Space: 44.9 MB (11.85%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -3,18 +3,17 @@ class Solution {
        int l = 0;
        int r = arr.length-1;
        int count = 0;
-       int ans = 0;
-       int ans1 = 0;
+       
        while(l<=r){
         int mid = l+(r-l)/2;
         count = arr[mid]-(mid+1);
         
         if(count<k){
-            ans = mid;
+            
             l = mid+1;
         }
         else {
-            ans1 = mid;
+            
             r = mid-1;
         }
        }
