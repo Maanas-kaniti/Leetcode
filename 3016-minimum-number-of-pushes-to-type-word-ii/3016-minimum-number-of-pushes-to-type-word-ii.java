4c84fee4ce2c23a5aa51bd6156359d129dca4f8c
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-07-31 11:01:29 +0530

    Time: 63 ms (13.01%), Space: 48.5 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -3,14 +3,14 @@ class Solution {
     public int minimumPushes(String word) {
         int count = 1;
         int c = 1;
-        Map<Character,Integer> map = new TreeMap<>(Collections.reverseOrder());
+        Map<Character,Integer> map = new HashMap<>();
         for(char n : word.toCharArray()){
             map.put(n,map.getOrDefault(n,0)+1);
         }
         List<Integer> l = new ArrayList<>(map.values());
         
         int ans = 0;
-        System.out.println(l.toString());
+        // System.out.println(l.toString());
         Collections.sort(l,Collections.reverseOrder());
         for(int i = 0;i<l.size();i++){
             if(count<8){
