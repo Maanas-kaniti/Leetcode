50a2ea5722da070b24a478409e23087db6c731e8
Maanas Kaniti <150051799+Maanas-kaniti@users.noreply.github.com>
2026-08-04 08:12:27 +0530

    Time: 3 ms (88.36%), Space: 48.4 MB (26.61%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,16 +1,34 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i = 0;i<matrix.length;i++){
-            int l = 0;
-            int r = matrix[i].length-1;
-            while(l<=r){
-                int mid = l+(r-l)/2;
-                if(matrix[i][mid]==target) return true;
-                else if(matrix[i][mid]>target){
-                    r = mid-1;
-                }
-                else l = mid+1;
+        //This is a better approach O(mlogn)
+        // for(int i = 0;i<matrix.length;i++){
+        //     int l = 0;
+        //     int r = matrix[i].length-1;
+        //     while(l<=r){
+        //         int mid = l+(r-l)/2;
+        //         if(matrix[i][mid]==target) return true;
+        //         else if(matrix[i][mid]>target){
+        //             r = mid-1;
+        //         }
+        //         else l = mid+1;
+        //     }
+        // }
+        // return false;
+
+
+
+        //this will be O(m+n);
+
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(target==matrix[row][col]){
+                return true;
             }
+            else if(target<matrix[row][col]){
+                col--;
+            }
+            else row++;
         }
         return false;
     }
